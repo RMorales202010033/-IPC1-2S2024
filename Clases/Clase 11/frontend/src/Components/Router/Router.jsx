9785dfc0c19eb4d100fd0bf32a9e97fe12f0c12c
RMorales202010033 <jrodolfomc2002@gmail.com>
2024-10-11 11:16:48 -0600

    Clase 11

---
+++ Clases/Clase 11/frontend/src/Components/Router/Router.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from '../Login';
+import { Admin } from '../Admin';
+import { LoadPosts } from '../LoadPosts';
+import Usuario from '../Usuario';
+import CreatePost from '../CreatePost';
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Creación de las distintas rutas que se tendrán (urls) y el componente que se llamará */}
+                <Route path='/login' element={<Login />} />
+                <Route path='/admin' element={<Admin />} />
+                <Route path='/loadPosts' element={<LoadPosts />} />
+                <Route path='/user' element={<Usuario />} />
+                <Route path='/createPost' element={<CreatePost />} />
+                <Route path='/*' element={<Navigate to="/login" />} />
+                <Route path='/' element={<Navigate to="/login" />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
